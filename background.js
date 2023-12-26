64ac9d1e57f2c8309d0ce86d68bea974a9fe2bd6
dhanushl0l <dhanushdjtherock@gmail.com>
2023-12-26 06:31:26 +0530

    debug

--- background.js
+++ background.js
@@ -1,10 +1,22 @@
-chrome.runtime.onInstalled.addListener(function () {
-    console.log("Media Control Extension Installed");
-  });
-  
-  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+chrome.commands.onCommand.addListener(function (command) {
+  if (command === "mediaForward") {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, request);
+      chrome.tabs.sendMessage(tabs[0].id, { action: "mediaForward" });
     });
+  }
+});
+
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            media: [{ media: "audio" }]
+          })
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+      }
+    ]);
   });
-  
+});
